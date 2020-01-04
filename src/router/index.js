3a8e028ce419a6caaa060d6bdc16e8fc98851d34
lbw <2659154921@qq.com>
2020-01-04 20:49:59 +0800

    完成个人中心页的创建，及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'//引入vue
 import vueRouter from 'vue-router'//引入vue-router
 import login from '@/views/login.vue'//登录页
+import personal from '@/views/personal.vue'//个人中心页
 //挂载
 Vue.use(vueRouter)
 //配置路由
 let router = new vueRouter({
     routes:[
+        //登陆页
         {
         name : 'login',
         path : '/login',
         component : login
+        },
+        //个人中心页
+        {
+            name : 'personal',
+            path : '/personal',
+            component : personal
         }
     ]
 })
---
+++ src/views/personal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grzx">
+ 个人中心页
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
