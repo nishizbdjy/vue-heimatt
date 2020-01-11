2b8248c33dd92fd8ff4699f723ec25ca355f5836
lbw <2659154921@qq.com>
2020-01-11 17:15:49 +0800

    完成主页头部结构

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="index">
-      主页
+    <!-- 头部 -->
+    <div class="tou">
+      <!-- logo -->
+      <div class="logo">
+        <span data-v-a83bd3b0 class="iconfont iconnew"></span>
+      </div>
+      <!-- 搜索 -->
+      <div class="sousuo">
+        <i data-v-a83bd3b0="" class="van-icon van-icon-search"></i>
+        <span>搜索新闻</span>
+      </div>
+      <!-- 个人中心 -->
+      <div class="grzx">
+        <i data-v-a83bd3b0 class="van-icon van-icon-manager-o"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.tou {
+  color:#fff;
+  width: 100vw;
+  height: 50px;
+  padding: 10px 10px;
+  background-color: red;
+  display:flex;
+  justify-content: space-around;
+  align-items: center;
+  .logo{
+    span{
+      font-size:50px;
+    }
+  }
+  .sousuo{
+    background-color:pink;
+    flex:0.8;
+    border-radius: 25px;
+    height: 100%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.6;
+  }
+  .grzx{
+    i{
+      font-size:30px
+    }
+  }
+}
 </style>
